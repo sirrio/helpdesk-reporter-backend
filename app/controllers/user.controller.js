97d133b404e086e7467dd8a4929c499106309d05
sirrio <home@sirrio.de>
2021-06-20 20:45:58 +0200

    Send role names with request

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -2,7 +2,7 @@ const db = require('../models');
 const User = db.user;
 
 exports.findAll = (request, response) => {
-    User.find().then(data => response.send(data)).catch(error => {
+    User.find().populate('roles', 'name').then(data => response.send(data)).catch(error => {
         console.log(error);
         response.status(500).send({
             message: error.message || 'error on user.findAll'
@@ -12,7 +12,7 @@ exports.findAll = (request, response) => {
 
 exports.findOne = (request, response) => {
     const id = request.params.id;
-    User.findById(id).then(data => {
+    User.findById(id).populate('roles', 'name').then(data => {
         if (data) response.send(data);
     }).catch(error => {
         response.status(500).send({message: error + `error on user.findOne with id=${id}`});
